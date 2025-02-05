b1532f37badb08977a33d79e77e4f23c296d73df
Woollywood <115595112+Woollywood@users.noreply.github.com>
2025-02-05 05:33:32 +0300

    feat: init axios and path aliases

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,5 @@
 import React from 'react';
 
 export const App: React.FC = () => {
-	return (
-		<div>
-			<h1 className='text-3xl font-bold underline'>Hello world!</h1>
-		</div>
-	);
+	return <div>Hello world</div>;
 };
